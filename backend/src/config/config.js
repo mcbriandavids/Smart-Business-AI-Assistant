4ded1d17e9d6ea62d7c6c9bcf81fd0e4b19d5d90
Imonisa Oghenekevwe Brian <42264681+mcbriandavids@users.noreply.github.com>
2025-10-14 20:42:48 +0100

    chore:set-default-port-3000

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -15,7 +15,7 @@ const config = {
   isProd,
 
   // Server
-  port: Number(process.env.PORT || 5000),
+  port: Number(process.env.PORT || 3000),
   frontendUrl: process.env.FRONTEND_URL || "http://localhost:3000",
 
   // Database
